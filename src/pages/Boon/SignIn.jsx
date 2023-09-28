7baed49f29be8a53fd30091e1d0be2281c1581f4
yodsaphonmonteerarat <122859465+yodsaphonmonteerarat@users.noreply.github.com>
2023-09-28 14:07:43 +0700

    merge all front end
    Khim KK and Boon create design page

--- src/pages/Boon/SignIn.jsx
+++ src/pages/Boon/SignIn.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import "./signin.css"
+import logoboon from "../Boon/picture/logoboon.png"
+
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>
-        Sign in page
-      </h1>
+    <div className='signinBackGround'>
+      <div className='signinLogoBox'>
+        <div className='signinBiskketLogo'><img src={logoboon} width="93px" height="88px" />
+          BISKKET
+        </div>
+      </div>
+      <div className='signinBox'>
+        <img className='signinLogo' src={logoboon} width="180px" height="120px" />
+        <div className='signinLogo'></div>
+      <button className='signinGoogle'>Signin with Google</button>
+
+        <div className='signinText'>Sign In To Enjoy<br></br> Biskket !</div>
+      </div>
     </div>
   )
 }
--- src/pages/KK/FriendProfile.jsx
+++ src/pages/KK/FriendProfile.jsx
@@ -1,12 +1,52 @@
 import React from 'react'
+import "./FriendProfile.css"
+import logo from "../KK/assets/logo.png"
+import home from "../KK/assets/home.png"
+import userprofile from "../KK/assets/userprofile.png"
+import like from "../KK/assets/like.png"
 
 const FriendProfile = () => {
   return (
-    <div>
-      <h1>
-        friend profile page !!!
-      </h1>
-    </div>
+    <div className='FriendProfileContainer'>
+      <div className='FriendProfileCookie'></div>
+
+      <div className='FriendProfileLogoBox'></div>
+      <div className='FriendProfileHomeBox'></div>
+      <img className='FriendProfileLogo' src={logo}></img>
+      <p class='BiskketText'>BISKKET</p>
+
+      <img className='FriendProfileHome' src={home}></img>
+      <p class='HomeText'>HOME</p>
+
+      <div className='FPUserBox'></div>
+      <img className='FPUserImage' src={userprofile}></img>
+      <div className='FPUserText'>username</div>
+
+      <div className='FPFirstBox'></div>
+      <div className='FPFirstImageBox'></div>
+      <div className='FPFirstCaption'>This will be the caption</div>
+      <img className='FPFirstLikeImg' src={like}></img>
+      <div className='FPFirstNumberLikes'>Likes : </div>
+
+      <div className='FPSecondBox'></div>
+      <div className='FPSecondImageBox'></div>
+      <div className='FPSecondCaption'>This will be the caption</div>
+      <img className='FPSecondLikeImg' src={like}></img>
+      <div className='FPSecondNumberLikes'>Likes : </div>
+
+      <div className='FPThirdBox'></div>
+      <div className='FPThirdCaptionBox'></div>
+      <div className='FPThirdCaption'>This will be the post caption</div>
+      <img className='FPThirdLikeImg' src={like}></img>
+      <div className='FPThirdNumberLikes'>Likes : </div>
+      
+      <div className='FPFourthBox'></div>
+      <div className='FPFourthImageBox'></div>
+      <div className='FPFourthCaption'>This will be the caption</div>
+      <img className='FPFourthLikeImg' src={like}></img>
+      <div className='FPFourthNumberLikes'>Likes : </div>
+      
+      </div>
   )
 }
 
