6183ff142d9f894dc582546ad13e552d86e33f0e
WEQAZ <62786101+WEQAZ@users.noreply.github.com>
2023-10-29 16:35:50 +0700

    config

--- src/config.jsx
+++ src/config.jsx
@@ -7,14 +7,14 @@ import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } f
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0juvhogtz_nAkDFPdtKhaEaljbxaIUCE",
-  authDomain: "web-project-11340.firebaseapp.com",
-  databaseURL: "https://web-project-11340-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "web-project-11340",
-  storageBucket: "web-project-11340.appspot.com",
-  messagingSenderId: "742883454635",
-  appId: "1:742883454635:web:cc69d98c30885d8d0111ee",
-  measurementId: "G-JXX47CYYV5"
+  apiKey: "AIzaSyA7cbCzTQGTH9GZ8yo7Ib6-hYlaY9Oqrgo",
+  authDomain: "biskket-12bc3.firebaseapp.com",
+  databaseURL: "https://biskket-12bc3-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "biskket-12bc3",
+  storageBucket: "biskket-12bc3.appspot.com",
+  messagingSenderId: "584075097253",
+  appId: "1:584075097253:web:19e9683c82977c8e2c094c",
+  measurementId: "G-SZL1ZMDKTP"
 };
 
 
